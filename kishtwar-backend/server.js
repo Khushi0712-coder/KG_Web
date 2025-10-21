b0a7f003f9ee3399244b8289e525411f50a2fc2f
KishtwarGold <khushi0712042@gmail.com>
2025-10-21 15:15:13 +0530

    KG

--- kishtwar-backend/server.js
+++ kishtwar-backend/server.js
@@ -66,7 +66,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 // Handle preflight OPTIONS requests
-app.options("*", cors(corsOptions));
+app.options("/*", cors(corsOptions));
 
 // =====================
 // API Routes
